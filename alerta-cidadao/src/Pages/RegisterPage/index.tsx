7d2ca2ff8d9c55857373372239f31a5bcb3121a5
Romero <ronerodocarmojunior@hotmail.com>
2023-03-06 15:35:23 -0300

    fix:routes

--- alerta-cidadao/src/Pages/RegisterPage/index.tsx
+++ alerta-cidadao/src/Pages/RegisterPage/index.tsx
@@ -1,6 +1,9 @@
 import { StyledRegisterPage } from './style'
 
 export const RegisterPage = () => {
+
+  console.log(' estou no register page ')
+  
   return (
     <StyledRegisterPage>
         <h1> Aqui vai o form do register  </h1>
