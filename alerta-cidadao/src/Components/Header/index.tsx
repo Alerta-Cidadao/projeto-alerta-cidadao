b0e5d089da2f3903e4f5e021510b55ff27ce05ca
Romero <ronerodocarmojunior@hotmail.com>
2023-03-06 16:17:06 -0300

    fix:home-page

--- alerta-cidadao/src/Components/Header/index.tsx
+++ alerta-cidadao/src/Components/Header/index.tsx
@@ -4,13 +4,19 @@ import { NavUnlogged } from '../NavUnlogged'
 import { NavLogged } from '../NavLogged'
 import { useContext } from 'react'
 import { UserContext } from '../../Context/userContext'
+import { useNavigate } from 'react-router-dom'
 export const Header = () => {
 
   const {user} =  useContext(UserContext)
-  
+  const navigate = useNavigate()
+
+  const goToHome = ()=>{
+    navigate('/home')
+  }
+
   return (
     <StyledHeader>
-        <img src={logoImg} alt="" />
+        <img src={logoImg} alt="" onClick={()=>{goToHome()}}/>
         {
           user ?
           <NavLogged/>
