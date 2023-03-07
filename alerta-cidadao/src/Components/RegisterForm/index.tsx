bd253e5197506cafa9faf3167f517624abd84cf5
MiguelC <mig1799@gmail.com>
2023-03-07 13:34:28 -0300

    feat: selecteds de estado e cidade no cadastro e minor-fix no userContex

--- alerta-cidadao/src/Components/RegisterForm/index.tsx
+++ alerta-cidadao/src/Components/RegisterForm/index.tsx
@@ -4,8 +4,27 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { schemaRegisterForm } from "./schema";
 import { Input } from "../Input";
 import { IRegisterFormData } from "../../Context/@types";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../Context/userContext";
+import axios from "axios";
+
+interface IUf {
+  id: number;
+  nome: string;
+  regiao: IRegiao;
+  sigla: string;
+}
+
+interface IRegiao {
+  id: number;
+  nome: string;
+  sigla: string;
+}
+
+interface IMunicipio {
+  id: number;
+  nome: string;
+}
 
 export const RegisterForm = () => {
   const { handleSubmitRegister } = useContext(UserContext);
@@ -14,6 +33,23 @@ export const RegisterForm = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<IRegisterFormData>({ resolver: yupResolver(schemaRegisterForm) });
+  const [ufs, setUfs] = useState<IUf[]>([]);
+  const [municipios, setMunicipios] = useState<IMunicipio[]>([]);
+  const [selectedUF, setSelectedUf] = useState("0");
+
+  useEffect(() => {
+    axios
+      .get("https://servicodados.ibge.gov.br/api/v1/localidades/estados/")
+      .then((response) => setUfs(response.data));
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUF}/municipios`
+      )
+      .then((response) => setMunicipios(response.data));
+  }, [selectedUF]);
 
   const submitRegister: SubmitHandler<IRegisterFormData> = (formData) => {
     handleSubmitRegister(formData);
@@ -26,7 +62,7 @@ export const RegisterForm = () => {
         type="text"
         placeholder="Digite seu user name"
         register={register("name")}
-        error={errors.email}
+        error={errors.name}
       />
       <Input
         label="Email"
@@ -46,10 +82,31 @@ export const RegisterForm = () => {
         label="Confirm password"
         type="password"
         placeholder="Confirme sua senha"
-        register={register("confirmpassword")}
-        error={errors.confirmpassword}
+        register={register("confirmPassword")}
+        error={errors.confirmPassword}
       />
-
+      <select
+        id="uf"
+        {...register("estado")}
+        onChange={(event) => {
+          setSelectedUf(event?.currentTarget.value);
+        }}
+      >
+        <option value="0">Selecione seu estado</option>
+        {ufs.map((uf) => (
+          <option key={uf.id} value={uf.sigla}>
+            {uf.nome}
+          </option>
+        ))}
+      </select>
+      <select id="city" {...register("cidade")}>
+        <option value="0">Selecione sua cidade</option>
+        {municipios.map((municipio) => (
+          <option key={municipio.id} value={municipio.nome}>
+            {municipio.nome}
+          </option>
+        ))}
+      </select>
       <button type="submit">Cadastrar</button>
     </StyledRegisterForm>
   );
